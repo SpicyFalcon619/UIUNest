6310ff4b91b2c227926eed6d3385095931e88ca0
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-05-25 23:17:35 +0600

    Clean up temporary testing scripts

--- test_seeking.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require 'api/db.php';
-$stmt = $pdo->query("SELECT * FROM seeking_posts ORDER BY created_at DESC LIMIT 5");
-$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($posts, JSON_PRETTY_PRINT);
